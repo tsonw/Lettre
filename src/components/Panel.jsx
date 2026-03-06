059c96010fab074a9c4d32984b86019580e87743
thoang3 <thai-son.hoang@universite-paris-saclay.fr>
2026-03-07 00:59:06 +0100

    Done

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,50 +1,59 @@
 import React, { useState } from "react";
 import "../styles/Panel.css";
 
+import hiGif from "../assets/hi.gif";
 import cuteGif from "../assets/cute.gif";
 import yesGif from "../assets/yes.gif";
 import nonoGif from "../assets/nono.gif";
+import ngaiGif from "../assets/ngai.gif";
 
 export function Panel (props) {
        
+       const [countStep, setCountStep] = useState(0);
        const [countClickButton1, setCountClickButton1] = useState(0);
        const [countClickButton2, setCountClickButton2] = useState(0);
 
+       const list_title_1 = [
+              "Hellu Chíp",
+              "This flower is for",
+              "Chíp Chíp 8/3 thiệt là vui vẻ và luôn xing gái nha."
+       ];
+       const list_title_2 = [
+              "8/3 em có cái này cho Chíp",
+              "the most beautiful flower",
+              ""
+       ];
+       const list_img = [
+              hiGif,
+              cuteGif,
+              ngaiGif
+       ];
+
        const checkButton1 = () => {
-              setCountClickButton1(prev => prev + 1)
+              setCountStep(prev => prev + 1)
               setCountClickButton2(0)
-              console.log(countClickButton1);
        }
        const checkButton2 = () => {
-              setCountClickButton2(prev => prev + 1)
-              console.log(countClickButton2);
+              setCountStep(prev => prev + 1)
        }
 
        return (
               <>     
                      <div className="panel_content">
-                            <h1>{countClickButton2 > 0 ? "" : "8/3"}</h1>
-                            <h1>{countClickButton2 > 0 ? "" : "Tặng Chíp bông hoa"}</h1>
-                            <img src={countClickButton2 > 0 ? nonoGif : cuteGif} alt="Cute" />
-                            <h1>{countClickButton2 > 0 ? "Không cho từ chối" : "Chíp nhận không ?"}</h1>
-                            <div className="panel_button">
-                                   <button 
-                                          className="button_accept"
-                                          type="button"
-                                          onClick={() => checkButton1()}
-                                   >
-                                          Nhận
-                                   </button>
-                                   {countClickButton2 == 0 ? (
+                            <h1>{list_title_1.at(countStep)}</h1>
+                            <img src={list_img.at(countStep)} className="img_panel" alt="Cute" />
+                            <h1>{list_title_2.at(countStep)}</h1>
+                            {countStep >= 2 ? "" :
+                                   <div className="panel_button">
                                           <button 
-                                                 className="button_cancel"
+                                                 className="button_accept"
                                                  type="button"
-                                                 onClick={() => checkButton2()}
+                                                 onClick={() => checkButton1()}
                                           >
-                                                 Không thèm
-                                          </button>) : ""
-                                   }
-                            </div>
+                                                 Tiếp
+                                          </button>
+                                   </div>
+                            }
                      </div>
               </>
        );
